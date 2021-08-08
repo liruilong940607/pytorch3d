check gradients (WIP -- find fw bug)

docs/examples/pulsar_basic.py
@@ -43,6 +43,8 @@ def cli():
     vert_pos[:, :2] -= 5.0
     vert_col = torch.rand(n_points, 3, dtype=torch.float32, device=device, requires_grad=True)
     vert_rad = torch.rand(n_points, dtype=torch.float32, device=device)
+    opacity = torch.ones_like(vert_rad) * 0.02
+    opacity.requires_grad = True
     cam_params = torch.tensor(
         [
             0.0,
@@ -65,10 +67,12 @@ def cli():
         cam_params,
         1.0e-1,  # Renderer blending parameter gamma, in [1., 1e-5].
         45.0,  # Maximum depth.
-        opacity=torch.ones_like(vert_rad) * 0.02,
+        opacity=opacity,
         mode=2,
     )
-    image.sum().backward()
+    # image.sum().backward()
+    # print (vert_col.grad)
+    # print (opacity.grad)
     LOGGER.info("Writing image to `%s`.", path.abspath("basic.png"))
     imageio.imsave("basic.png", (image.cpu().detach() * 255.0).to(torch.uint8).numpy())
     LOGGER.info("Done.")